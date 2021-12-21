fe7466a83b932813b810a26bea65a242feef837f
Frantisek_Kurimsky <feri.kurimsky@gmail.com>
2021-12-21 02:55:50 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import axios, {AxiosResponse} from "axios";
+import {Router, NavigationEnd,ActivatedRoute} from '@angular/router';
+// import * as Stomp from 'stompjs';
+// import * as SockJS from 'sockjs-client';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  platforms = [{},{},{},{},{}];
+  platforms2 = [
+    [{    }],
+    [{    }],
+    [{    }],
+    [{    }],
+    [{    }]
+  ];
+  trains = [];
+  displayedColumns: string[] = ['type', 'departure', 'arrival', 'delay', 'platform'];
+  displayedColumns2: string[] = ['type', 'departure', 'arrival', 'delay'];
   title = 'table';
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+
+    setInterval(() => {
+      axios.get('http://localhost:8000/all').then((res) => {
+        this.trains = res.data
+        // console.log(res)
+      })
+      axios.get('http://localhost:8000/platforms').then((res) => {
+        this.platforms2 = [
+          [{    }],
+          [{    }],
+          [{    }],
+          [{    }],
+          [{    }]
+        ];
+        this.platforms = res.data
+        for (let i = 0; i < 5; i++) {
+          this.platforms2[i]=res.data.filter((x: any) => x.platform == i+1);
+          // console.log(res.data.filter((x: any) => x.platform == i+1))
+        }
+
+        // console.log(res)
+      })
+
+    }, 2000)
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatTableModule} from '@angular/material/table';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatTableModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
